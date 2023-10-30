74a6060bf452eda898cda611ae42b9cb4eddb78a
Bernardo <bernardo.o2006@aluno.ifsc.edu.br>
2023-10-30 10:21:48 -0300

    Update Atividades

--- BancoGrupo1.sql
+++ BancoGrupo1.sql
@@ -538,7 +538,47 @@ update Hospedagens
 set checkin = '10/10/2023', checkout = '15/10/2023'
 where  hospedagens_id = 10;
 
+/*update atividades*/
 
+update Atividades
+set nome_atividade='aula de boxe'
+where id_atividade=1;
+
+update Atividades
+set nome_atividade ='aula de vôlei'
+where id_atividade=11;
+
+update Atividades
+set nome_atividade= 'aula de capoeira'
+where id_atividade=7;
+
+update Atividades
+set nome_atividade='aula de futebol'
+where id_atividade=12;
+
+update Atividades
+set nome_atividade='pôker'
+where id_atividade=18;
+
+update Atividades
+set nome_atividade='aula de karatê'
+where id_atividade=5;
+
+update Atividades
+set nome_atividade='aula de kickbox'
+where id_atividade=9;
+
+update Atividades
+set nome_atividade='piscina'
+where id_atividade=7;
+
+update Atividades
+set nome_atividade='sauna'
+where id_atividade=19;
+
+update Atividades
+set nome_atividade='bugy'
+where id_atividade=20;
 
 
 
