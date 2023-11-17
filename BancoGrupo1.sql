1a17723fd6837bd083d3d3c2f3c70a1b63117cca
Bernardo <bernardo.o2006@aluno.ifsc.edu.br>
2023-11-17 11:20:37 -0300

    Hospedagens Quartos Funcionando

--- BancoGrupo1.sql
+++ BancoGrupo1.sql
@@ -427,7 +427,7 @@ values(1,1,1),
 
 /*Tabela hospedagens_quartos*/
 
-insert into Hospedagens_quartos (id_hospede, id_quartos, id_hospedagens)
+insert into Hospedagens_quartos (id_hospede, id_quartos, Hospedagens_id)
 values(1,1,1),
 (2,2,2),
 (3,3,3),
